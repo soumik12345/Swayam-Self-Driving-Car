b1606f352f2a9bd5e68a88e39377ea579a2dcab5
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2018-01-03 11:04:50 +0530

    Fixed some bugs in LaneFinder.py
    Fixed some bugs in LaneFinder.py like avoiding false positives and division by zero.

--- LaneFinder.py
+++ LaneFinder.py
@@ -1,6 +1,6 @@
 # Importing Libraries
 import numpy as np
-from numpy import ones, vstack
+from numpy import ones, vstack, zeros_like
 from numpy.linalg import lstsq
 from statistics import mean
 import cv2, pyautogui
@@ -13,13 +13,13 @@ class LaneFinder:
     def process(self, image):
     
         def region_of_interest(img, vertices):
-            mask = np.zeros_like(img) # blank mask
-            cv2.fillPoly(mask, vertices, 255)
-            return cv2.bitwise_and(img, mask)
+            mask = zeros_like(img) # Empty mask  
+            cv2.fillPoly(mask, vertices, 255) # Adding the polygon inside the mask
+            return cv2.bitwise_and(img, mask) # Joining the mask and the image
         
         def draw_lanes(img, lines, color=[0, 255, 255], thickness=3):
             try:
-                ys = []  
+                ys = []  # Determining the horizon
                 for i in lines:
                     for ii in i:
                         ys += [ii[1],ii[3]]
@@ -28,13 +28,17 @@ class LaneFinder:
                 new_lines = []
                 line_dict = {}
                 for idx,i in enumerate(lines):
-                    for xyxy in i:
-                        x_coords = (xyxy[0],xyxy[2])
-                        y_coords = (xyxy[1],xyxy[3])
+                    for cord in i:
+                        x_coords = (cord[0], cord[2])
+                        y_coords = (cord[1], cord[3])
                         A = vstack([x_coords,ones(len(x_coords))]).T
                         m, b = lstsq(A, y_coords)[0]
-                        x1 = (min_y-b) / m
-                        x2 = (max_y-b) / m
+                        x1, x2 = 0, 0
+                        if m!=0:
+                            x1 = (min_y-b) / m
+                            x2 = (max_y-b) / m
+                        else: # Avoiding division by zero
+                            x1, x2 = 9223372036854775807, 9223372036854775807
                         line_dict[idx] = [m,b,[int(x1), min_y, int(x2), max_y]]
                         new_lines.append([int(x1), min_y, int(x2), max_y])
                 final_lanes = {}
@@ -63,10 +67,7 @@ class LaneFinder:
                 lane1_id = top_lanes[0][0]
                 lane2_id = top_lanes[1][0]
                 def average_lane(lane_data):
-                    x1s = []
-                    y1s = []
-                    x2s = []
-                    y2s = []
+                    x1s, y1s, x2s, y2s = [], [], [], []
                     for data in lane_data:
                         x1s.append(data[2][0])
                         y1s.append(data[2][1])
@@ -78,32 +79,29 @@ class LaneFinder:
                 return [l1_x1, l1_y1, l1_x2, l1_y2], [l2_x1, l2_y1, l2_x2, l2_y2]
             except Exception as e:
                 print(str(e))
+                pass
         
-        original_image = image
-        image=cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        vertices = [np.array([[10,500],[10,300],[300,200],[500,200],[800,300],[800,500]], np.int32)]
-        gray = region_of_interest(cv2.cvtColor(image, cv2.COLOR_BGR2GRAY), vertices)
-        edge = cv2.Canny(gray, threshold1 = 200, threshold2 = 300)
-        blur = cv2.GaussianBlur(edge, (5, 5), 0)
-        lines = cv2.HoughLinesP(blur, 1, np.pi/180, 180, 20, 15)
+        original_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        image = cv2.Canny(image, threshold1 = 200, threshold2=300)
+        image = cv2.GaussianBlur(image,(5,5),0)
+        vertices = np.array([[10,500],[10,300],[300,200],[500,200],[800,300],[800,500]], np.int32)
+        image = region_of_interest(image, [vertices])
+        lines = cv2.HoughLinesP(image, 1, np.pi/180, 180,      20,       15)
         try:
-            lane1, lane2 = draw_lanes(image, lines)
-            cv2.line(image, (lane1[0], lane1[1]), (lane1[2], lane1[3]), [0, 255, 0], 10)
-            cv2.line(image, (lane2[0], lane2[1]), (lane2[2], lane2[3]), [0, 255, 0], 10)
+            l1, l2 = draw_lanes(original_image,lines)
+            cv2.line(original_image, (l1[0], l1[1]), (l1[2], l1[3]), [0,255,0], 30)
+            cv2.line(original_image, (l2[0], l2[1]), (l2[2], l2[3]), [0,255,0], 30)
         except Exception as e:
-            print(str(e))
             pass
-        
         try:
-            for i in lines:
-                coords=i[0]
+            for coords in lines:
+                coords = coords[0]
                 try:
-                    cv2.line(image, (coords[0], coords[1]), (coords[2], coords[3]), [255, 0, 0], 10)
+                    cv2.line(image, (coords[0], coords[1]), (coords[2], coords[3]), [255,0,0], 3)
                 except Exception as e:
                     print(str(e))
-                    pass
         except Exception as e:
-            print(str(e))
             pass
+        return original_image
         
-        return image
