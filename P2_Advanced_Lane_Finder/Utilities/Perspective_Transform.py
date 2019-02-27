17556f0bc24b9642901873a30fe8a347fa1c51dc
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2019-02-28 02:08:50 +0530

    Added Perspective Transformation

---
+++ P2_Advanced_Lane_Finder/Utilities/Perspective_Transform.py
@@ -0,0 +1,67 @@
+import warnings
+warnings.filterwarnings('ignore')
+import numpy as np
+import cv2, glob, os, pickle
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from Utilities.Undistort import *
+
+class Perspective_Transform:
+
+    def __init__(self, image):
+        self.image = image
+    
+    def undistort(image):
+        undistort = Undistort(image, model_path = './models/')
+        return undistort.process()
+    
+    def get_roi(image, vertices):
+        vertices = np.array(vertices, ndmin = 3, dtype = np.int32)
+        mask = np.zeros_like(image)
+        mask = cv2.fillPoly(mask, vertices, (255, 255, 255))
+        return cv2.bitwise_and(image, mask)
+    
+    def warp_image(image, warp_shape, source, destination):
+        M = cv2.getPerspectiveTransform(source, destination)
+        inverse_M = cv2.getPerspectiveTransform(destination, source)
+        warped = cv2.warpPerspective(image, M, warp_shape, flags = cv2.INTER_LINEAR)
+        return warped, M, inverse_M
+    
+    def perspective_transform(image):
+        y, x, _ = image.shape
+        source = np.float32([
+            (696, 455),    
+            (587, 455), 
+            (235, 700),  
+            (1075, 700)
+        ])
+        destination = np.float32([
+            (x - 350, 0),
+            (350, 0),
+            (350, y),
+            (x - 350, y)
+        ])
+        image_labelled = image.copy()
+        cv2.polylines(image_labelled, [np.int32(source)], True, (255, 0, 0), 3)
+        warped, M, invM = Perspective_Transform.warp_image(image, (x, y), source, destination)
+        warped_labelled = warped.copy()
+        cv2.polylines(warped_labelled, [np.int32(destination)], True, (255, 0, 0), 3)
+        vertices = np.array([
+            [200, y],
+            [200, 0],
+            [1100, 0],
+            [1100, y]
+        ])
+        roi = Perspective_Transform.get_roi(warped, vertices)
+        return warped, warped_labelled, roi, image_labelled
+    
+    def process(self):
+        Perspective_Transform.undistort = staticmethod(Perspective_Transform.undistort)
+        Perspective_Transform.get_roi = staticmethod(Perspective_Transform.get_roi)
+        Perspective_Transform.warp_image = staticmethod(Perspective_Transform.warp_image)
+        Perspective_Transform.perspective_transform = staticmethod(Perspective_Transform.perspective_transform)
+
+        undistorted = Perspective_Transform.undistort(self.image)
+        warped, warped_labelled, roi, image_labelled = Perspective_Transform.perspective_transform(undistorted)
+
+        return warped, warped_labelled, roi, image_labelled
--- P2_Advanced_Lane_Finder/main.py
+++ P2_Advanced_Lane_Finder/main.py
@@ -5,11 +5,22 @@ import cv2, glob, os, pickle
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from Utilities.Undistort import *
+from Utilities.Perspective_Transform import *
 
 undistort = Undistort(
-    mpimg.imread('./Chessboards/8x6/test_image.jpg'),
+    mpimg.imread('./Lane Images/straight_lines2.jpg'),
     model_path = './models/'
 )
 undistorted = undistort.process()
 plt.imshow(undistorted)
+plt.title('Undistorted')
+plt.show()
+
+perspective_transform = Perspective_Transform(mpimg.imread('./Lane Images/straight_lines2.jpg'))
+warped, warped_labelled, roi, image_labelled = perspective_transform.process()
+plt.imshow(image_labelled)
+plt.title('Labelled Image')
+plt.show()
+plt.imshow(warped_labelled)
+plt.title('Birds Eye View')
 plt.show()
