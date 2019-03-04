73255ca867ed3fb883b57f0a2a4f7fc532de3037
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2019-03-04 05:35:04 +0530

    Lane Detection on Real World Footage

--- P2_Advanced_Lane_Finder/main.py
+++ P2_Advanced_Lane_Finder/main.py
@@ -1,41 +1,67 @@
 import warnings
 warnings.filterwarnings('ignore')
 import numpy as np
-import cv2, glob, os, pickle
+import cv2, glob, os, pickle, sys
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-from Utilities.Undistort import *
 from Utilities.Perspective_Transform import *
-from Utilities.Binarize import *
 from Utilities.Lane_Detector import *
+os.system('cls')
+print('Libraries Imported')
 
-undistort = Undistort(
-    mpimg.imread('./Lane Images/straight_lines2.jpg'),
-    model_path = './models/'
-)
-undistorted_image = undistort.process()
-plt.imshow(undistorted_image)
-plt.title('Undistorted')
-plt.show()
+def load_model(file):
+    file = open(file, 'rb')
+    _object = pickle.load(file)
+    file.close()
+    return _object['mtx'], _object['dist']
 
-perspective_transform = Perspective_Transform(undistorted_image)
-warped, warped_labelled, roi, image_labelled, M, invM = perspective_transform.process()
-plt.imshow(image_labelled)
-plt.title('Labelled Image')
-plt.show()
-plt.imshow(warped_labelled)
-plt.title('Birds Eye View')
-plt.show()
+def hsl_white_yellow(image):
+    # convert to hls
+    hsl = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
+    # white mask
+    lower = np.uint8([  0, 200,   0])
+    upper = np.uint8([255, 255, 255])
+    white = cv2.inRange(hsl, lower, upper)
+    # yellow mask
+    lower = np.uint8([ 10,   0, 100])
+    upper = np.uint8([ 40, 255, 255])
+    yellow = cv2.inRange(hsl, lower, upper)
+    # Combining the mask and the image
+    mask = cv2.bitwise_or(white, yellow)
+    return mask
 
-binarize = Binarize(warped)
-binarized_image = binarize.process()
-plt.imshow(binarized_image, cmap = 'gray')
-plt.title('Binarized Image')
-plt.show()
+def pipeline(frame, mtx, dist):
+    undistorted_image = cv2.undistort(frame, mtx, dist, None, mtx)
+    perspective_transformer = Perspective_Transform(undistorted_image)
+    warped, warped_labelled, roi, image_labelled, M, invM = perspective_transformer.process()
+    binary_image = hsl_white_yellow(warped)
+    lane_detector = Lane_Detector(frame, binary_image, M, invM)
+    sliding_window_image, result, final_image, left_fit_x, right_fit_x, plot_y, left_curve_radius, right_curve_radius = lane_detector.process()
+    return final_image, left_curve_radius, right_curve_radius
 
-lane_detector = Lane_Detector(undistorted_image, binarized_image, M, invM)
-sliding_window_image, result, final_image, left_fit_x, right_fit_x, plot_y, left_curve_radius, right_curve_radius = lane_detector.process()
-plt.imshow(final_image)
-plt.title('Lane Detection')
-plt.xlabel('Left Radius Of Curvature: ' + str(left_curve_radius) + '\nRight Radius Of Curvature: ' + str(right_curve_radius))
-plt.show()
+def main():
+    mtx, dist = load_model('./models/camera_callibration.p')
+    print('Camera Model Loaded')
+    if sys.argv[1] == 'V':
+        cap = cv2.VideoCapture(sys.argv[2])
+        out = cv2.VideoWriter(sys.argv[3], -1, 20.0, (int(cap.get(3)), int(cap.get(4))))
+        print('capture created')
+        while cap.isOpened():
+            ret, frame = cap.read()
+            try:
+                rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                final_image, left_curve_radius, right_curve_radius = pipeline(rgb_frame, mtx, dist)
+                final_image = cv2.cvtColor(final_image, cv2.COLOR_RGB2BGR)
+                cv2.putText(final_image, 'Left Curve Radius:' + str(left_curve_radius), (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 1)
+                cv2.putText(final_image, 'Right Curve Radius:' + str(right_curve_radius), (30, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 1)
+                out.write(final_image)
+            except:
+                break
+            cv2.imshow(sys.argv[2] + '_final_output', final_image)
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                cap.release()
+                out.release()
+                cv2.destroyAllWindows()
+                break
+
+main()
